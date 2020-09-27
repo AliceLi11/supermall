644d35ee4e98cab431eaa5cb5d64b042887e38f9
AliceLi11 <1148127906@qq.com>
2020-09-27 18:26:17 +0800

    我的

---
+++ src/views/profile/childComps/ListView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="list">
+    <div v-for="(item,index) in listData" :key="index" class="list-item">
+      <p class="icon">
+        <svg :fill="item.iconColor"><use :xlink:href="item.icon"></use></svg>
+      </p>
+      <div class="info">{{item.info}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"ListView",
+  props:{
+    listData:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #list{
+    background-color: #fff;
+    font-size: 15px;
+    color: #333;
+  }
+  .list-item{
+    height: 44px;
+    line-height: 44px;
+    position: relative;
+  }
+  .icon{
+    position: absolute;
+    margin-top: 10px;
+    left: 16px;
+    top: -7px;
+  }
+  .icon svg {
+    width: 18px;
+    height: 18px;
+  }
+  .info{
+    margin-left: 40px;
+    border-bottom: 1px solid rgba(100,100,100,.1);
+    padding-left: 5px;
+  }
+  .list-item:last-of-type .info{
+    border-bottom: none;
+  }
+</style>
---
+++ src/views/profile/childComps/UserInfo.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="user-info">
+    <a href="#" class="clear-fix user-info-box">
+      <div class="user-icon">
+        <slot name="user-icon">
+          <svg class="privateImage-svg left">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#avatar-default"></use>
+          </svg>
+        </slot>
+      </div>
+      
+      <div class="login-info left">
+        <slot name="user-nickname">
+          <div>登录/注册</div>
+        </slot>
+        <div class="phone">
+          <span>
+            <svg data-v-735ff1be="" fill="#fff" class="icon-mobile"><use data-v-735ff1be="" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#mobile"></use></svg>
+          </span>
+          <div class="user-phone">
+            <slot name="user-phone">暂无绑定手机号</slot>
+          </div>
+        </div>
+      </div>
+
+      <div class="arror-right">
+        <svg data-v-735ff1be="" fill="#fff" class="arrow-svg right"><use data-v-735ff1be="" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use></svg>
+      </div>
+      
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"UserInfo"
+}
+</script>
+
+<style scoped>
+  .user-info {
+    background-color: var(--color-tint);
+    padding: 15px;
+    margin-top: -5px;
+  }
+
+  .user-info-box{
+    display: flex;
+    align-items: center;
+  }
+  .user-icon{
+    width:70px;
+  }
+  .arror-right{
+    width:20px;
+    text-align: right;
+  }
+  .login-info{
+    flex:1;
+  }
+ .user-info .privateImage-svg {
+    width: 60px;
+    height: 60px;
+    background-color: #fff;
+    border-radius: 30px;
+  }
+
+  .user-info .arrow-svg {
+    width: 11px;
+    height: 22px;
+  }
+
+  .user-info .login-info {
+    color: #fff;
+  }
+
+  .user-info .login-info .phone {
+    position: relative;
+    font-size: 13px;
+    margin-top: 5px;
+    font-weight: 300;
+  }
+
+  .user-phone{
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .user-info .login-info .phone .icon-mobile {
+    width: 12px;
+    height: 18px;
+    vertical-align: middle;
+  }
+</style>
